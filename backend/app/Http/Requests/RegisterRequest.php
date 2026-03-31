4e2d3565abbcc08e12474cbccbc6dd40caa86ee1
Bouchaib El Haidi <bouchaib@example.com>
2026-03-31 03:47:17 +0100

    implemented form validation for registerrequest

---
+++ backend/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'post' => 'required|string|max:120',
+            'department_id' => 'required|exists:departments,id',
+        ];
+    }
+}
