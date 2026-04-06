d6ff44c547c8f59c50a653d0e554ca14260b74ac
Bouchaib El Haidi <bouchaib@example.com>
2026-04-06 16:11:21 +0100

    added migration for request_items_table table

---
+++ backend/database/migrations/2026_04_09_182012_create_request_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('productName');
+            $table->string('reference')->nullable();
+            $table->integer('quantity');
+            $table->unsignedBigInteger('purchase_request_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('request_items');
+    }
+};
